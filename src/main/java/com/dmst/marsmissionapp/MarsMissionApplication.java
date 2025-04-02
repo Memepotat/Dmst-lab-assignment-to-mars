6de6a10232a6341d69b6e16c47a8967dc4bf36b8
Nikos Damalos <ndamalos1967@gmail.com>
2025-04-03 01:42:53 +0300

    Added CRUD API

---
+++ src/main/java/com/dmst/marsmissionapp/MarsMissionApplication.java
@@ -0,0 +1,13 @@
+package com.dmst.marsmissionapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsMissionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsMissionApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/dmst/marsmissionapp/ProductController.java
@@ -0,0 +1,35 @@
+package com.dmst.marsmissionapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductRepository repository;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return repository.findAll();
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        return repository.save(product);
+    }
+
+    @PutMapping("/{id}")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        product.setId(id);
+        return repository.save(product);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProduct(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
