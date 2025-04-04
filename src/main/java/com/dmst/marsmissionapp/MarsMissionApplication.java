7283470a10b4d8ff6db1e8ba3881f8f7f839c62f
Nikos Damalos <ndamalos1967@gmail.com>
2025-04-04 17:44:49 +0300

    Added compose both back and front

--- src/main/java/com/dmst/marsmissionapp/MarsMissionApplication.java
+++ src/main/java/com/dmst/marsmissionapp/MarsMissionApplication.java
@@ -2,6 +2,9 @@ package com.dmst.marsmissionapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MarsMissionApplication {
@@ -10,4 +13,16 @@ public class MarsMissionApplication {
 		SpringApplication.run(MarsMissionApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+						.allowedHeaders("*");
+			}
+		};
+	}
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO product (name, quantity) VALUES ('Oxygen Tank', 20);
+INSERT INTO product (name, quantity) VALUES ('Hydration Pack', 50);
+INSERT INTO product (name, quantity) VALUES ('Food Rations', 100);
